ca3c1a509391d2018efe4b70f498a5b81b5ec7f3
Mihael <mihaelmiki123@live.com>
2021-12-19 19:29:42 +0100

    .

--- src/routes/posts.json.js
+++ src/routes/posts.json.js
@@ -2,7 +2,7 @@ import client from "$lib/db";
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function get({ query }) {
-    return {app_id:typeof(VITE_MONGODB_APP_ID)!=='undefined' ? VITE_MONGODB_APP_ID : null, api_key:typeof(VITE_MONGODB_API_KEY)!=='undefined' ? VITE_MONGODB_API_KEY : null};
+    return {body:{app_id:typeof(VITE_MONGODB_APP_ID)!=='undefined' ? VITE_MONGODB_APP_ID : null, api_key:typeof(VITE_MONGODB_API_KEY)!=='undefined' ? VITE_MONGODB_API_KEY : null}};
     const all = query.has('all') && !!query.get('all');
     if(all) {
         return (await client.db('abc').collection('blogposts').find());
